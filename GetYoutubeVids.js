729c273c9d7da5e5c9c33046f06631549751b837
BuildTools <unconfigured@null.spigotmc.org>
2016-10-25 16:24:53 -0600

    da fuck

--- GetYoutubeVids.js
+++ GetYoutubeVids.js
@@ -3,21 +3,21 @@
  */
 
 function getVids() {
-    
-    $.getJSON("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCI3U4iYLLnc5DSordUnL_HlX1SdGgkmv4&channelId=UCQOwWvXpgO4NNgKzyYoi3-A&part=snippet,id&order=date&maxResults=5", function (data) {
-       $.each (data.items, function (i, v) {
-           var id = "http://www.youtube.com/embed/"+(v.id.videoId);
-           var ifrm = document.createElement("iframe");
-           ifrm.setAttribute("src", id);
-           ifrm.style.width = "400px";
-           ifrm.style.height = "200px";
-           ifrm.style.border = "0";
-           ifrm.style.alignItems = "center";
-           document.getElementById("vids").appendChild(ifrm);
 
-       })
-    
-     
+    $.getJSON("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCI3U4iYLLnc5DSordUnL_HlX1SdGgkmv4&channelId=UCQOwWvXpgO4NNgKzyYoi3-A&part=snippet,id&order=date&maxResults=5", function (data) {
+        $.each (data.items, function (i, v) {
+            var id = "http://www.youtube.com/embed/"+(v.id.videoId);
+            var ifrm = document.createElement("iframe");
+            ifrm.setAttribute("src", id);
+            ifrm.style.width = "400px";
+            ifrm.style.height = "200px";
+            ifrm.style.border = "0";
+            ifrm.style.alignItems = "center";
+            document.getElementById("vids").appendChild(ifrm);
+
+        })
+
+
     });
 }
 
