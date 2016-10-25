67d62bf54402f42ef906ef848c0db3e88162ee32
BuildTools <unconfigured@null.spigotmc.org>
2016-10-25 11:22:45 -0600

    I FUCKING DID IT

---
+++ GetYoutubeVids.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Keith on 10/25/2016.
+ */
+
+function getVids() {
+    
+    $.getJSON("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCI3U4iYLLnc5DSordUnL_HlX1SdGgkmv4&channelId=UCQOwWvXpgO4NNgKzyYoi3-A&part=snippet,id&order=date&maxResults=5", function (data) {
+       $.each (data.items, function (i, v) {
+           var id = "http://www.youtube.com/embed/"+(v.id.videoId);
+           var ifrm = document.createElement("iframe");
+           ifrm.setAttribute("src", id);
+           ifrm.style.width = "640px";
+           ifrm.style.height = "480px";
+           document.body.appendChild(ifrm);
+
+       })
+    
+     
+    });
+}
+
