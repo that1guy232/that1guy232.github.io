5840bd0f30912b163973d1485d3725877e2261a5
keith <keithmaccuish5@gmail.com>
2017-11-23 18:29:52 -0700

    Update GetYoutubeVids.js
    https != http

--- GetYoutubeVids.js
+++ GetYoutubeVids.js
@@ -6,7 +6,7 @@ function getVids() {
 
     $.getJSON("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCI3U4iYLLnc5DSordUnL_HlX1SdGgkmv4&channelId=UCQOwWvXpgO4NNgKzyYoi3-A&part=snippet,id&order=date&maxResults=5", function (data) {
         $.each (data.items, function (i, v) {
-            var id = "http://www.youtube.com/embed/"+(v.id.videoId);
+            var id = "https://www.youtube.com/embed/"+(v.id.videoId);
             var ifrm = document.createElement("iframe");
             ifrm.setAttribute("src", id);
             ifrm.style.width = "350px";
